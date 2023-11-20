343228cadbf380524107f4c43d07a4e61a39157c
don_koteyka_mac <777zik@gmail.com>
2023-11-20 19:21:02 +0300

    fixed git.yml

--- 2.1-databases/work_with_database/phones/migrations/0001_initial.py
+++ 2.1-databases/work_with_database/phones/migrations/0001_initial.py
@@ -14,7 +14,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Phone',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                                           primary_key=True,
+                                           serialize=False,
+                                           verbose_name='ID')),
                 ('name', models.CharField(max_length=50)),
                 ('price', models.IntegerField()),
                 ('image', models.CharField(max_length=100)),
